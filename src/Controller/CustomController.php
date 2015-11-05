b6236f4c58e9764f7552122735686964d8e504cc
dkgndec <dkpawankumar8@gmail.com>
2015-11-05 15:36:42 +0530

    adding new function

--- src/Controller/CustomController.php
+++ src/Controller/CustomController.php
@@ -8,10 +8,22 @@ use Drupal\Core\Controller\ControllerBase;
 
 class CustomController extends ControllerBase {
     public function content() {
+        $markup = t('this is davinder first module') . $this->snehi();
         return array(
             '#type' => 'markup',
-            '#markup' => t('this is davinder first module'),
+            '#markup' =>  $markup ,
+            '#title' => $this->t('Argument Information'),
         );
+
+    }
+    public function snehi() {
+        $mark = "";
+        for($i=1;$i<=10;$i++)
+        {
+            $mark .= '<h2>'.$this->t('Heading '.$i).'</h2>';
+            $mark .= '<p>This is the descriptin '.$i.'</p>';
+        }
+        return $mark;
     }
 }
 ?>
